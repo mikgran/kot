6b094d4063df135df8809224f42f286c060106a8
Mika Granqvist <mika.granqvist@outlook.com>
2018-11-15 16:24:38 +0200

    Added RgenResource
    Removed test class.

--- kot/rgen/src/main/kotlin/mg/kot/rgen/RgenResource.kt
+++ kot/rgen/src/main/kotlin/mg/kot/rgen/RgenResource.kt
@@ -3,8 +3,8 @@ package mg.kot.rgen
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 
-@Path("helloWorld")
-class HelloWorldResource {
+@Path("rgen")
+class RgenResource {
     @GET
     fun helloWorld() = "Hello World"
 }
