c0dd907d792dfea9aab5189a658e4adbe1004925
Mika Granqvist <mika.granqvist@outlook.com>
2018-11-23 16:09:41 +0200

    Added missing BiOpt2.

---
+++ kot/util/src/main/kotlin/mg/util/functional/BiOpt2.kt
@@ -0,0 +1,76 @@
+package mg.util.functional
+
+class BiOpt2<T : Any, V : Any>(l: Opt2<T>, r: Opt2<V>) {
+
+    private val left: Opt2<T> = l
+    private val right: Opt2<V> = r
+
+    fun left() = left
+    fun right() = right
+
+    fun <R : Any, V : Any> match(ref: R,
+                                 filter: (R?) -> Boolean,
+                                 mapper: (R) -> V): BiOpt2<T, V> {
+
+        return left.match(ref, filter, mapper)
+    }
+
+    fun <R : Any, V : Any> matchRight(ref: R,
+                                      filter: (R?) -> Boolean,
+                                      mapper: (R) -> V): BiOpt2<T, V> {
+
+        val matchedRight = right.match(ref, filter, mapper)
+
+        @Suppress("UNCHECKED_CAST")
+        return BiOpt2.of(right, matchedRight.right()) as BiOpt2<T, V> // the new match situation is old A new B type, force into T, V
+    }
+
+    fun caseOf(predicate: (T) -> Boolean,
+               mapper: (T) -> V): BiOpt2<T, V> {
+
+        return filter { !right.isPresent() }
+                .filter { left.isPresent() }
+                .filter { predicate(left.get() as T) }
+                .left()
+                .map(mapper)
+                .map { newRight -> BiOpt2(left, Opt2(newRight)) }
+                .getOrElse(this)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun filter(filter: (T) -> Boolean): BiOpt2<T, V> = when {
+
+        left.isPresent() && filter(left.get() as T) -> this
+        else -> BiOpt2.empty()
+    }
+
+    fun getLeftOrElseThrow(exceptionProducer: () -> Throwable): T? {
+        return when {
+            left.isPresent() -> left.get()
+            else -> throw exceptionProducer()
+        }
+    }
+
+    fun getRightOrElseThrow(exceptionProducer: () -> Throwable): V? {
+        return when {
+            right.isPresent() -> right.get()
+            else -> throw exceptionProducer()
+        }
+    }
+
+    companion object Factory {
+
+        @JvmStatic
+        fun <T : Any, V : Any> of(t: T?, v: V?): BiOpt2<T, V> = BiOpt2(Opt2.of(t), Opt2.of(v))
+
+        @JvmStatic
+        fun <T : Any, V : Any> of(t: Opt2<T>, v: Opt2<V>): BiOpt2<T, V> = when {
+            t.isPresent() || v.isPresent() -> BiOpt2(t, v)
+            else -> empty()
+        }
+
+        @JvmStatic
+        fun <T : Any, V : Any> empty(): BiOpt2<T, V> = BiOpt2(Opt2.empty(), Opt2.empty())
+    }
+
+}
