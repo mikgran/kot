f6610a96e6b5eb97b6b9757154750bc7f80b37f8
Mika Granqvist <mika.granqvist@outlook.com>
2019-11-14 18:18:55 +0200

    Refactored Sql.kt functions: generalization.

--- kot/util/src/main/kotlin/mg/util/db/Sql.kt
+++ kot/util/src/main/kotlin/mg/util/db/Sql.kt
@@ -28,7 +28,7 @@ abstract class BuildingBlock {
 data class SelectBlock<T>(override val blocks: MutableList<BuildingBlock>, val type: T) : BuildingBlock() {
 
     infix fun <T : KProperty1<*, *>> where(type: T): WhereBlock<T> {
-        return getAndCacheWhereBlock(blocks, type)
+        return getAndCacheWhereBlock(type, blocks) { t, b -> WhereBlock(b, t) }
     }
 
     override fun toString(): String {
@@ -39,9 +39,7 @@ data class SelectBlock<T>(override val blocks: MutableList<BuildingBlock>, val t
 data class WhereBlock<T : KProperty1<*, *>>(override val blocks: MutableList<BuildingBlock>, val type: T) : BuildingBlock() {
 
     infix fun <T> eq(type: T): OperationBlock<T> {
-        val operationBlock = OperationBlock(blocks, type)
-        blocks.add(operationBlock)
-        return operationBlock
+        return getAndCacheWhereBlock(type, blocks) { t, b -> OperationBlock(b, t) }
     }
 
     override fun toString(): String {
@@ -64,14 +62,12 @@ data class UpdateBlock<T>(override val blocks: MutableList<BuildingBlock>, val t
     }
 
     infix fun <T : KProperty1<*, *>> where(type: T): WhereBlock<T> {
-        return getAndCacheWhereBlock(blocks, type)
+        return getAndCacheWhereBlock(type, blocks) { t, b -> WhereBlock(b, t) }
     }
-
-
 }
 
-private fun <T : KProperty1<*, *>> getAndCacheWhereBlock(blocks: MutableList<BuildingBlock>, type: T): WhereBlock<T> {
-    val whereBlock = WhereBlock(blocks, type)
-    blocks.add(WhereBlock(blocks, type))
-    return whereBlock
+private fun <T, R: BuildingBlock> getAndCacheWhereBlock(type: T, list: MutableList<BuildingBlock>, f: (type: T, list: MutableList<BuildingBlock>) -> R): R {
+    val block = f(type, list)
+    list.add(block)
+    return block
 }
