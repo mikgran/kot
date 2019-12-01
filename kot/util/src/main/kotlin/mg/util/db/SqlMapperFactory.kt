1133af7ac21255e8d4a1336f5a7925c7632b7124
Mika Granqvist <mika.granqvist@outlook.com>
2019-12-01 17:52:59 +0200

    Cleanup.

--- kot/util/src/main/kotlin/mg/util/db/SqlMapperFactory.kt
+++ kot/util/src/main/kotlin/mg/util/db/SqlMapperFactory.kt
@@ -3,10 +3,9 @@ package mg.util.db
 import mg.util.db.dsl.oracle.Sql as OrSql
 import mg.util.db.dsl.mysql.Sql as MySql
 
-
 object SqlMapperFactory {
 
-    fun get(type : String?) : SqlMapper = when (type) {
+    fun get(type: String?): SqlMapper = when (type) {
         "mysql" -> SqlMapper(MySql())
         "oracle" -> SqlMapper(OrSql())
         else -> SqlMapper(MySql())
