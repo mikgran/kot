0742d1618130a7455d83e577dd91f6bf06ab2c62
Mika Granqvist <mika.granqvist@outlook.com>
2019-12-01 17:34:57 +0200

    Added base sql file.

---
+++ kot/util/src/main/kotlin/mg/util/db/Sql.kt
@@ -0,0 +1,24 @@
+package mg.util.db
+
+import mg.util.db.dsl.BuildingBlock
+import mg.util.db.dsl.mysql.CreateBlock
+import mg.util.db.dsl.mysql.SelectBlock
+import mg.util.db.dsl.mysql.UpdateBlock
+
+abstract class Sql {
+
+    open fun <T : BuildingBlock> newCachedBlock(mapper: (blocks: MutableList<BuildingBlock>) -> T): T {
+        val list = mutableListOf<BuildingBlock>()
+        val buildingBlock = mapper(list)
+        list.add(buildingBlock)
+        return buildingBlock
+    }
+
+    open fun <T : Any> newSelect(list: MutableList<BuildingBlock>, t: T) = SelectBlock(list, t)
+    open fun <T : Any> newUpdate(list: MutableList<BuildingBlock>, t: T) = UpdateBlock(list, t)
+    open fun <T : Any> newCreate(list: MutableList<BuildingBlock>, t: T) = CreateBlock(list, t)
+
+    open infix fun <T : Any> select(t: T): SelectBlock<T> = newCachedBlock { list -> newSelect(list, t) }
+    open infix fun <T : Any> update(t: T): UpdateBlock<T> = newCachedBlock { list -> newUpdate(list, t) }
+    open infix fun <T : Any> create(t: T): CreateBlock<T> = newCachedBlock { list -> newCreate(list, t) }
+}
