4500e7a71a0df7c52ea7ff42f9476aee6f0e4f1b
Mika Granqvist <mika.granqvist@outlook.com>
2019-12-06 14:51:31 +0200

    Merged More..

--- kot/util/src/main/kotlin/mg/util/db/dsl/mysql/CreateBlock.kt
+++ kot/util/src/main/kotlin/mg/util/db/dsl/mysql/CreateBlock.kt
@@ -3,6 +3,7 @@ package mg.util.db.dsl.mysql
 import mg.util.db.dsl.BuildingBlock
 import mg.util.db.dsl.DslParameters
 import mg.util.functional.Opt2.Factory.of
+import java.lang.reflect.Field
 import kotlin.reflect.full.declaredMemberProperties
 
 open class CreateBlock<T : Any>(override val blocks: MutableList<BuildingBlock>, open val type: T) : BuildingBlock(type) {
@@ -12,24 +13,49 @@ open class CreateBlock<T : Any>(override val blocks: MutableList<BuildingBlock>,
         // type (f1, f2, f3, custom1, collection<custom2>)
         // create type (f1, f2, f3), buildForParent(custom1), buildForParent(collection<custom2>)
 
-        val lists = dp.typeT!!::class.java.declaredFields
+        val bb = getCustomObjects(dp)
+
+        val cc = getListsOfCustomObjects(dp)
+                .map { buildSqlForCustomCollections(dp, it) }
+
+//        lists.flatten()
+//                .filterNotNull()
+//                .distinctBy { it::class.java.packageName + it::class.java.simpleName }
+//                .apply(::println)
+        return buildSqlForParent(dp)
+    }
+
+    private fun getCustomObjects(parentDslParameters: DslParameters): List<Any> {
+
+        typeOfParent(parentDslParameters)
+                .declaredFields
+                .filterNot(::isList)
+
+        return emptyList()
+    }
+
+    private fun typeOfParent(parentDslParameters: DslParameters): Class<out Any> =
+            parentDslParameters.typeT!!::class.java
+
+    private fun buildSqlForCustomCollections(parentDslParameters: DslParameters, listsOfCustomsObjects: List<*>): String {
+
+
+        return ""
+    }
+
+    private fun getListsOfCustomObjects(dp: DslParameters): List<List<*>> {
+        return typeOfParent(dp).declaredFields
                 .filterNotNull()
-                .filter { List::class.java.isAssignableFrom(it.type) }
+                .filter(::isList)
                 .map {
                     it.isAccessible = true
                     it.get(dp.typeT) as List<*>
                 }
-
-        lists.flatten()
-                .filterNotNull()
-                .distinctBy { it::class.java.packageName + it::class.java.simpleName }
-                .apply(::println)
-
-
-        return createSimple(dp)
     }
 
-    private fun createSimple(dp: DslParameters): String {
+    private fun isList(it: Field) = List::class.java.isAssignableFrom(it.type)
+
+    private fun buildSqlForParent(dp: DslParameters): String {
         val sqlFieldDefinitionsCommaSeparated = of(dp)
                 .map(::buildSqlFieldDefinitions)
                 .map { it.joinToString(", ") }
