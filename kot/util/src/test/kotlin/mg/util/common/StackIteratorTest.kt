14fa5cf5b2d2f6611485738c0f991bef4d7cee7a
Mika Granqvist <mika.granqvist@outlook.com>
2020-01-14 14:08:58 +0200

    Added test coverage. Cleanup.

--- kot/util/src/test/kotlin/mg/util/common/StackIteratorTest.kt
+++ kot/util/src/test/kotlin/mg/util/common/StackIteratorTest.kt
@@ -6,34 +6,51 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class StackIteratorTest {
 
+    private val list1 = listOf(1, 2, 3, 4, 5)
+    private val list2 = listOf(6, 7)
+    private val list3 = listOf(8, 9, 10)
+    private val list4 = listOf(11)
+
     @Test
-    fun testHasNext() {
-        // TOIMPROVE: test coverage
+    fun testHasNextAndNext() {
+
+        val iterator = list2.stackIterator()
+
+        assertTrue(iterator.hasNext())
+        assertEquals(6, iterator.next())
+        assertTrue(iterator.hasNext())
+        assertEquals(7, iterator.next())
+        assertFalse(iterator.hasNext())
+        assertThrows(NoSuchElementException::class.java) {
+            iterator.next()
+        }
     }
 
-
     @Test
-    fun testNext() {
+    fun testStackIterator() {
+
+        testStackOperatorFor {
+            list1.stackIterator() +
+                    list2.iterator() +
+                    list3.iterator() +
+                    list4.iterator()
+        }
     }
 
     @Test
-    fun testPlus() {
+    fun testStackIteratorWithOperatorPlus() {
+
+        testStackOperatorFor {
+            list1.iterator() +
+                    list2.iterator() +
+                    list3.iterator() +
+                    list4.iterator()
+        }
     }
 
-    @Test
-    fun testStackIterator1() {
-
-        val list1 = listOf(1, 2, 3, 4, 5)
-        val list2 = listOf(6, 7)
-        val list3 = listOf(8, 9, 10)
-        val list4 = listOf(11)
-
-        val stackIterator: StackIterator<Int> = list1.stackIterator() + list2.iterator() + list3.iterator() + list4.iterator()
-
+    private fun testStackOperatorFor(stackIteratorSupplier: () -> StackIterator<Int>) {
         val candidateList = mutableListOf<Int>()
-
-        stackIterator.forEach { candidateList.add(it) }
-
+        stackIteratorSupplier().forEach { candidateList.add(it) }
         assertEquals((1..11).toList(), candidateList)
     }
 }
