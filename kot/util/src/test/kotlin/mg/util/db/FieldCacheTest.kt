f038d7f5c121d8a73762813010bcbf8041904c30
Mika Granqvist <mika.granqvist@outlook.com>
2021-06-06 17:23:59 +0300

    Added FieldCache.fieldsFor

--- kot/util/src/test/kotlin/mg/util/db/FieldCacheTest.kt
+++ kot/util/src/test/kotlin/mg/util/db/FieldCacheTest.kt
@@ -12,11 +12,20 @@ internal class FieldCacheTest {
 
         val person = FCPerson("full name", FCAddress("SomeAddress"), mutableListOf(FCBill(10, "${Date()}")))
 
+        // this fails on parallel tests
+        assertEquals(0, FieldCache.cache.size)
+
         val fields = FieldCache.fieldsFor(person)
 
         assertTrue(fields.primitives.any { it.name == "fullName" })
         assertTrue(fields.customs.any { it.name == "address" })
-        assertTrue(fields.listsOFCustoms.any { it.name == "bills" })
+        assertTrue(fields.listsOfCustoms.any { it.name == "bills" })
+        assertEquals(1, fields.primitives.size)
+        assertEquals(1, fields.customs.size)
+        assertEquals(1, fields.listsOfCustoms.size)
+        assertTrue(FieldCache.cache.containsKey(person))
+        // this fails on parallel tests
+        assertEquals(1, FieldCache.cache.size)
     }
 }
 
