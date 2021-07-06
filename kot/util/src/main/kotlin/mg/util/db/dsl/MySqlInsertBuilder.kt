a2b57b173da806e74d784beaef0c03f2a199f115
Mika Granqvist <mika.granqvist@outlook.com>
2021-07-06 18:07:16 +0300

    Renamed a method for clarity.

--- kot/util/src/main/kotlin/mg/util/db/dsl/MySqlInsertBuilder.kt
+++ kot/util/src/main/kotlin/mg/util/db/dsl/MySqlInsertBuilder.kt
@@ -16,13 +16,13 @@ class MySqlInsertBuilder {
         FieldAccessor.uniquesByParent(type).toOpt()
                 .mapWhen({ it.isEmpty() }) { hashMap -> hashMap[type] = emptyList(); hashMap }
                 .x {
-                    entries.forEach { buildParentAndItsChildSqls(it, sqls) }
+                    entries.forEach { buildSqlsForParentAndItsChildren(it, sqls) }
                 }
 
         return sqls.joinToString(";").also { println(it) }
     }
 
-    private fun buildParentAndItsChildSqls(entry: MutableMap.MutableEntry<Any, List<Any>>, sqls: MutableList<String>) {
+    private fun buildSqlsForParentAndItsChildren(entry: MutableMap.MutableEntry<Any, List<Any>>, sqls: MutableList<String>) {
         val parent = entry.key
         parent.toOpt()
                 .map(FieldCache::fieldsFor)
