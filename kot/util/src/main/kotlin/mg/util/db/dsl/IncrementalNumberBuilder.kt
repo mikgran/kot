e52b5b67a33b78577101acbda1d97dfe6d21008a
Mika Granqvist <mika.granqvist@outlook.com>
2021-08-04 17:52:36 +0300

    Renamed IncrementalIdBuilder.

--- kot/util/src/main/kotlin/mg/util/db/dsl/IncrementalNumberBuilder.kt
+++ kot/util/src/main/kotlin/mg/util/db/dsl/IncrementalNumberBuilder.kt
@@ -12,8 +12,14 @@ class IncrementalIdBuilder {
                 .ifEmpty { 0 }
                 .map { it + 1 }
                 .c { cache[str] = it }
-                .get()!!
+                .value()
     }
+
+    // return key + cache[key] or null if no key found
+    fun id(key: String): String? =
+            cache[key].toOpt()
+                    .map { key + it }
+                    .get()
 
     operator fun get(key: String): Int? = cache[key]
 
